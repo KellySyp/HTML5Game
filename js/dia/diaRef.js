150c465759def205dcdd136c3d96a1f53705da7c
kelly Syp <kelmsyp@gmail.com>
2019-04-14 19:54:07 -0400

    weeknd changes

--- js/dia/diaRef.js
+++ js/dia/diaRef.js
@@ -17,18 +17,18 @@ var diaNPC = {
 	"townb": "Nobody who goes into the forest comes back.",
 	"townc": "You can't go out empty handed.\nBuy Something?\n{Yes.} CALL-openShop(0)\n{No thanks.} GOTO-bad\nThanks\n-bad\nToo bad.",
 	"foresta": "Welcome to the forest outpost.\nBuy Something?\n{Yes.} CALL-openShop(1)\n{No thanks.} GOTO-bad\nThanks\n-bad\nToo bad.",
-	"forestb": "When's lunch?",
-	"forestc": "Zombies suck!",
+	"forestb": "Congratulations on making it this far.",
+	"forestc": "The zombie scourge started 5 years ago.\nEvery winter since has been like this.",
 	"wastea": "Welcome to the Wasteland Outpost.\nBuy Something?\n{Yes.} CALL-openShop(2)\n{No thanks.} GOTO-bad\nThanks\n-bad\nToo bad.",
-	"wasteb": "Being a guard is boring.",
-	"wastec": "I feel like a big fat pizza!",
+	"wasteb": "The butcher's daughter went missing 5 years ago.",
+	"wastec": "It's been a while since we've seen an adveturer out this far.\nIt sure does get lonely out here.",
 	"butcher": "I am the butcher.",
 	"queen": "I am the Zombie Queen.",
 	
 };
-/* var diaNPC = {
-	"oneLine": "This is one line!",
-	"multiLine": "\nThis is multiple lines\n#I hope everyone is Well!\nYAY!\n-next",
+ var diaObjects = {
+	"door": "It's locked\nCALS-checkKey('Key')\n-notHave\nIt's still locked\n-doesHave\nYou unlocked the door\nCALL-addGold(10)",
+	/*"chest": "\nThis is multiple lines\n#I hope everyone is Well!\nYAY!\n-next",
 	"testGoto": "\nHi. Let's skip the small talk.\nGOTO-end\nI hope everyone is Well!\nYAY!\n-end\nSee isn't that easy?\n-more",
 	"random": "-rdm0\nHi How are you?\n-rdm1\nToday is a beautiful day!\nIsn't it?\n-rdm2\nThe birds are singing and the sun is shining.\n-rdm3\nREDRUMREDRUMREDRUM!!!!",
 	"repeat": "Let me tell you a story!\nIt's a really good one!!\neh... I forgot.",
@@ -40,9 +40,8 @@ var diaNPC = {
 	"testAudio": "Hello, let's make some noise!\nCALL-playAudio('Coin.wav')\nThanks!",
 	"testMove": "Hello, what do you want me to do?\n{Follow Me} CALL-changeMove(2)\n{Go Away!} CALL-changeMove(3)\n{Whatever you want.} CALL-changeMove(1)\n{Stop} CALL-changeMove(0)\n#Comment!\nok!",
 	"testLabelCall": "Hello, pick a card!\n{Heart} GOTO-Heart\n{Club} GOTO-Club\n{Spade} GOTO-Spade\n{Diamond} GOTO-Diamond\n-Heart\nCALL-changeMove(2)\nI love you!\n-Club\nCALL-playAudio('Coin.wav')\nThanks!\n-Spade\nYou picked Spade\n-Diamond\nIf I was a rich bot! NAH NAH NAH",
-	"iterated": "-dia1\nHola!\n-dia2\nThis is the second line.\n-dia3\nThis is the third Line.\n-dia4\nThat's all I got."
-	
-}; */
+	"iterated": "-dia1\nHola!\n-dia2\nThis is the second line.\n-dia3\nThis is the third Line.\n-dia4\nThat's all I got."*/
+}; 
 
 var diaEvent = {
 	"test": "Hi\nI am a static event.\nDid I work?",
